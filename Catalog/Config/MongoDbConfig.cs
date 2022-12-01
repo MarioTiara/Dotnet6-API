c35b697d3a15624337f58c970e702dd53cd71cd3
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-02 00:31:17 +0700

    Add Config -> Mongodb Config

---
+++ Catalog/Config/MongoDbConfig.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Config
+{
+    public class MongoDbConfig
+    {
+        public string Connectionstring { get; set; }
+        public string DatabaseName {get;set;}
+        
+    }
+}

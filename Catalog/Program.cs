5ca6d2cfe5e94bdd61e3161b1dceac2505d469d3
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-02 00:33:17 +0700

    Inject Config, Inject IItemrepo with mongo repo

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,14 +1,23 @@
+using Catalog.Config;
 using Catalog.Repositories;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
-
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer( new DateTimeOffsetSerializer(BsonType.String));
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IItemRepository, InMemItemsRepository>();
+builder.Services.AddSingleton<IItemRepository, MongoDbRepository>();
+builder.Services.Configure<MongoDbConfig>(
+    builder.Configuration.GetSection("MongoDbConfig")
+);
 
 var app = builder.Build();
 
@@ -26,3 +35,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+

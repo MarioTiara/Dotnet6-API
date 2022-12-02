696cc4b5fc6640d0306f04f43be8958d2139e1ed
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-02 09:42:18 +0700

    Modify config to make env var friendly

--- Catalog/Config/MongoDbConfig.cs
+++ Catalog/Config/MongoDbConfig.cs
@@ -7,8 +7,15 @@ namespace Catalog.Config
 {
     public class MongoDbConfig
     {
-        public string Connectionstring { get; set; }
+        public string Host { get; set; }
+        public int Port {get;set;}
         public string DatabaseName {get;set;}
-        
+        public string User {get;set;}
+        public string Password {get;set;}
+        public string ConnectionString{
+            get{
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
+            }
+        }
     }
 }

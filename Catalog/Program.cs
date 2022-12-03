afe300a5df7a486c320144d1242acf303cc83542
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-03 15:33:13 +0700

    db Connection healtcheck endpoint and live service healtcheck endpoint

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,5 +1,6 @@
 using Catalog.Config;
 using Catalog.Repositories;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -9,16 +10,27 @@ var builder = WebApplication.CreateBuilder(args);
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer( new DateTimeOffsetSerializer(BsonType.String));
 // Add services to the container.
-
+var provider=builder.Services.BuildServiceProvider();
+var configuration=provider.GetRequiredService<IConfiguration>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IItemRepository, MongoDbRepository>();
+
 builder.Services.Configure<MongoDbConfig>(
     builder.Configuration.GetSection("MongoDbConfig")
 );
-builder.Services.AddHealthChecks();
+
+var mongoDbConfig=configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
+builder.Services.AddHealthChecks()
+    .AddMongoDb(
+        mongoDbConfig.ConnectionString,
+        name:"mongodb",
+        timeout:TimeSpan.FromMilliseconds(3),
+        tags:new []{"ready"}
+    );
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,7 +45,13 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-app.MapHealthChecks("health");
+
+app.MapHealthChecks("/health/ready", new HealthCheckOptions{
+    Predicate= (check)=>check.Tags.Contains("ready")
+});
+app.MapHealthChecks("/health/live", new HealthCheckOptions{
+    Predicate= (_)=>false
+});
 app.Run();
 
 

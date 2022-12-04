42b7d5d025c71c2db59b97af4e5ab5beb9875cbe
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-04 22:39:03 +0700

    Add GetItems_WithExistingItems_ReturnsAllItems

--- Catalog/Catalog.UnitTests/Controllers/ItemsControllerTets.cs
+++ Catalog/Catalog.UnitTests/Controllers/ItemsControllerTets.cs
@@ -18,7 +18,7 @@ namespace Catalog.UnitTests.Controllers
         private readonly Mock<ILogger<ItemsController>> loggerStub= new ();
         private readonly Random rand= new();
         [Fact]
-        public async Task GetItems_WithUnexistingItem_ReturnsNotFound(){
+        public async Task GetItem_WithUnexistingItem_ReturnsNotFound(){
             //Arrange
             repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
                 .ReturnsAsync((Item)null);
@@ -32,7 +32,7 @@ namespace Catalog.UnitTests.Controllers
         }
 
         [Fact]
-        public async Task GetItems_WithExistingItem_ReturnsExpectedItem(){
+        public async Task GetItem_WithExistingItem_ReturnsExpectedItem(){
             // Arrange
             Item expectedItem = CreateRandomItem();
 
@@ -48,6 +48,33 @@ namespace Catalog.UnitTests.Controllers
             Assert.IsType<ItemDto>(result.Value);
             result.Value.Should().BeEquivalentTo(expectedItem);
         }
+
+        [Fact]
+        public async Task GetItems_WithExistingItems_ReturnsAllItems(){
+            // Arrange
+            var expectedItem = new [] {
+                CreateRandomItem(),
+                CreateRandomItem(),
+                CreateRandomItem(),
+                CreateRandomItem()
+            };
+            repositoryStub.Setup(repo => repo.GetItemsAsync())
+                .ReturnsAsync(expectedItem);
+
+            var controller= new ItemsController(repositoryStub.Object, loggerStub.Object);
+            // Act
+            var actualItems= await controller.GetItems();
+
+            // Assert
+            Assert.Equal(expectedItem.Count(), actualItems.Count());
+            Assert.IsType<List<ItemDto>>(actualItems);
+            actualItems.Should().BeEquivalentTo(
+                expectedItem,
+                options=>options.ComparingByMembers<Item>()
+            );
+            
+        }
+
 
 
         private Item CreateRandomItem(){

be462d3899ecf6b8baa9ddd97f4462732625ba19
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-04 17:40:52 +0700

    Add app.UseHttpsRedirection

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -38,8 +38,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 
 app.UseHttpsRedirection();
